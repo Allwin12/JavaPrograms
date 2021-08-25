1b220703278b6823c022ccc5b34926a5c3be756c
Allwin Raju <allwindicaprio@gmail.com>
2021-08-25 11:06:07 +0530

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataType
+{
+	public static void main(String[] args) {
+		String string = "string";
+		System.out.println(string.getClass().getName());
+    
+    // java.lang.String
+	}
+}
